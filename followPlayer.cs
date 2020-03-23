8462d562f0e4697225795f2582d28e06a773f1dd
Ethan Chiu <ethanchiu@Ethande-MacBook-Air-2.local>
2020-03-23 22:11:14 +0800

    C# camera movement Unity

---
+++ followPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour {
+
+    public Vector3 dis;
+    public Transform player;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = player.position + dis;
+	}
+}
